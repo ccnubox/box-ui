1cc16c1e08c0931ea85c714bda22d29f971e2279
zxc0328 <120797887@qq.com>
2018-08-20 20:58:09 +0800

    finish notification

--- common/notification/index.js
+++ common/notification/index.js
@@ -5,38 +5,50 @@ import Touchable from "rax-touchable";
 import styles from "./index.css";
 import request from "../../util/request";
 import { createElement, Component } from "rax";
+const native = require("@weex-module/test");
 
 class Notification extends Component {
   state = {
-    text: "xxx",
-    show: true
+    text: "",
+    show: false,
+    type: "alert", // alert/link
+    btnText: "关闭",
+    link: ""
   };
   componentWillMount() {
-    // request({
-    //   method: "GET",
-    //   url: "xxxx"
-    // })
-    //   .then(data => {
-    //     this.setState({
-    //       text: data
-    //     });
-    //   })
-    //   .catch(err => {
-    //     //
-    //   });
+    request({
+      method: "GET",
+      url: `https://ccnubox.muxixyz.com/api/msg/?os=ios&page=${
+        this.props.pageId
+      }`
+    })
+      .then(data => {
+        this.setState({
+          type: data.version || "alert",
+          btnText: data.time || "关闭",
+          link: data.detail || "",
+          text: data.msg,
+          show: true
+        });
+      })
+      .catch(err => {});
   }
-  hide = () => {
-    this.setState({
-      show: false
-    });
+  onBtnPressed = () => {
+    if (this.state.type === "alert") {
+      this.setState({
+        show: false
+      });
+    } else if (this.state.type === "link") {
+      native.push(this.state.link);
+    }
   };
   render() {
     if (this.state.text === "" || !this.state.show) return null;
     return (
       <View style={styles.container}>
         <Text style={styles.text}>{this.state.text}</Text>
-        <Touchable onPress={this.hide}>
-          <Text style={styles.text}>关闭</Text>
+        <Touchable onPress={this.onBtnPressed}>
+          <Text style={styles.text}>{this.state.btnText}</Text>
         </Touchable>
       </View>
     );
